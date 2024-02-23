9de2fca0941a14b45b0ebe7ad503c8f92ddc6c03
Nick Neessen <nick.neessen@gmail.com>
2024-02-23 08:21:50 -0600

    ERRORS

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {
   useNavigation,
 } from "react-router-dom";
 import Home from "./ui/Home";
+import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
@@ -14,6 +15,7 @@ import AppLayout from "./ui/AppLayout";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -23,6 +25,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
