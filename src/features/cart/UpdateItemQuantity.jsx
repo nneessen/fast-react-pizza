c5386ec0ccba92fae2d7a920ad6feed144eb4710
Nick Neessen <nick.neessen@gmail.com>
2024-02-24 18:26:24 -0600

    a

---
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -0,0 +1,26 @@
+import { useDispatch } from 'react-redux';
+import Button from '../../ui/Button';
+import { decreaseItemQuantity, increaseItemQuantity } from './cartSlice';
+
+export default function UpdateItemQuantity({ pizzaId, currentQuantity }) {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex items-center gap-1 md:gap-3">
+      <Button
+        type="round"
+        onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
+      >
+        -
+      </Button>
+      <span className="font-md text-medium gap-2">{currentQuantity}</span>
+
+      <Button
+        type="round"
+        onClick={() => dispatch(increaseItemQuantity(pizzaId))}
+      >
+        +{' '}
+      </Button>
+    </div>
+  );
+}
